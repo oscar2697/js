46d769792be6e17fb574acf6f5214d0de63c69ee
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2022-05-19 17:53:44 -0500

    Add files via upload

---
+++ ejercicio.js
@@ -0,0 +1,17 @@
+const nombre = [1, "Oscar", true, undefined, null];
+const edad = 24;
+const desarrollador = true;
+
+console.log(nombre);
+console.log(edad);
+console.log(desarrollador);
+
+const fecha_cadena = new Date("november  26 1997");
+console.log(fecha_cadena);
+
+const libro = {
+    tiutlo: "Muerte en el Nilo",
+    autor: "Agatha Christie",
+    fecha: "november 1 1937",
+}
+console.log(movil);
